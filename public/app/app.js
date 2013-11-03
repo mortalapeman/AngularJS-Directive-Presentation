79065856471838b1ece64c2f073a6947d2c01033
Eric Pritchett <eric.a.pritchett.che@gmail.com>
2013-11-03 02:05:14 -0600

    Got visibility working

--- public/app/app.js
+++ public/app/app.js
@@ -1,1 +1,13 @@
 angular.module('app', [])
+
+.directive('prVisible', function() {
+  return function(scope, el, attrs) {
+    scope.$watch(attrs.prVisible, function(val) {
+      if (val) {
+        el.css('visibility', 'visible')
+      } else {
+        el.css('visibility', 'hidden')
+      }
+    });
+  };
+});
