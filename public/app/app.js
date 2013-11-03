b33dbcf94096203cdd455562f3c57b25b9b91f1a
Eric Pritchett <eric.a.pritchett.che@gmail.com>
2013-11-03 13:45:59 -0600

    Got a good example of a simple repeater going

--- public/app/app.js
+++ public/app/app.js
@@ -1,1 +1,36 @@
 angular.module('app', [])
+
+.controller('MainCtrl', function($scope) {
+  $scope.things = ['A', 'B', 'C'];
+  $scope.add = function(item) {
+    $scope.things.push(item);
+  }
+})
+
+.directive('simpleRepeat', function() {
+  return {
+    priority: 1000,
+    terminal: true,
+    transclude: 'element',
+    compile: function(el, attr, transclude) {
+      return function(scope, el, attr) {
+        var cleanup = [];
+        scope.$watch(attr.simpleRepeat, function(val) {
+          angular.forEach(cleanup, function(fn) { fn(); });
+          cleanup = [];
+          angular.forEach(val, function(x) {
+            var child = scope.$new();
+            child[attr.binding] = x;
+            transclude(child, function(clone) {
+              cleanup.push(function() {
+                child.$destroy();
+                clone.remove();
+              });
+              el.before(clone);
+            });
+          });
+        }, true);
+      }
+    }
+  }
+})
