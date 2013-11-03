ebd4d175aa8bb609e378981f0776ddc22aaa5456
Eric Pritchett <eric.a.pritchett.che@gmail.com>
2013-11-03 11:49:12 -0600

    User name form and check mark

--- public/app/app.js
+++ public/app/app.js
@@ -1,1 +1,26 @@
 angular.module('app', [])
+
+.directive('plNoduplicate', function() {
+  var usernames = ["Bob", "Harry"]
+  return {
+    require: 'ngModel',
+    link: function(scope, el, attrs, cntrl) {
+      cntrl.$parsers.push(function(val) {
+        if (usernames.indexOf(val) !== -1) {
+          cntrl.$setValidity('noduplicate', false);
+          return undefined;
+        }
+        cntrl.$setValidity('noduplicate', true);
+        return val;
+      })
+    }
+  };
+})
+
+.directive('tick', function() {
+  return {
+    restrict: 'E',
+    template: '<span>&#10003</span>',
+    replace: true
+  };
+})
